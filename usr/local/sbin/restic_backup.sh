532b7b4a610960393e4bbff79eeca6a8aeeae5d7
Nick Gaswirth <admin@roundhouse-designs.com>
2020-05-08 19:14:05 -0400

    testing

--- usr/local/sbin/restic_backup.sh
+++ usr/local/sbin/restic_backup.sh
@@ -9,35 +9,38 @@ set -e -o pipefail
 # If killed by systemd, like $(systemctl stop restic), then it kills the whole cgroup and all it's subprocesses.
 # However if we kill this script ourselves, we need this trap that kills all subprocesses manually.
 exit_hook() {
-	echo "In exit_hook(), being killed" >&2
-	jobs -p | xargs kill
-	restic unlock
+  echo "In exit_hook(), being killed" >&2
+  jobs -p | xargs kill
+  restic unlock
 }
 trap exit_hook INT TERM
 
 # How many backups to keep.
-RETENTION_DAYS=14
-RETENTION_WEEKS=16
-RETENTION_MONTHS=18
-RETENTION_YEARS=3
+RETENTION_DAYS=7
+RETENTION_WEEKS=8
+RETENTION_MONTHS=1
+RETENTION_YEARS=1
 
 # What to backup, and what to not
-BACKUP_PATHS="/ /boot /home"
-[ -d /mnt/media ] && BACKUP_PATHS+=" /mnt/media"
-BACKUP_EXCLUDES="--exclude-file /etc/restic/backup_exclude"
-for dir in /home/*
-do
-	if [ -f "$dir/.backup_exclude" ]
-	then
-		BACKUP_EXCLUDES+=" --exclude-file $dir/.backup_exclude"
-	fi
+BACKUP_PATHS=
+for d in /srv/rhdwp/www/*; do
+  BACKUP_PATHS+="$d "
 done
 
-BACKUP_TAG=systemd.timer
+# [ -d /mnt/media ] && BACKUP_PATHS+=" /mnt/media"
+BACKUP_EXCLUDES="--exclude-file /etc/restic/backup_exclude"
+for dir in /home/*; do
+  if [ -f "$dir/.backup_exclude" ]; then
+    BACKUP_EXCLUDES+=" --exclude-file $dir/.backup_exclude"
+  fi
+done
 
+BACKUP_TAG=cron.d
 
 # Set all environment variables like
 # B2_ACCOUNT_ID, B2_ACCOUNT_KEY, RESTIC_REPOSITORY etc.
+# shellcheck disable=SC1091
+# shellcheck source=/etc/restic/b2_env.sh
 source /etc/restic/b2_env.sh
 
 # How many network connections to set up to B2. Default is 5.
@@ -56,28 +59,29 @@ wait $!
 # See restic-backup(1) or http://restic.readthedocs.io/en/latest/040_backup.html
 # --one-file-system makes sure we only backup exactly those mounted file systems specified in $BACKUP_PATHS, and thus not directories like /dev, /sys etc.
 # --tag lets us reference these backups later when doing restic-forget.
+# shellcheck disable=SC2086
 restic backup \
-	--verbose \
-	--one-file-system \
-	--tag $BACKUP_TAG \
-	--option b2.connections=$B2_CONNECTIONS \
-	$BACKUP_EXCLUDES \
-	$BACKUP_PATHS &
+  --verbose \
+  --one-file-system \
+  --tag $BACKUP_TAG \
+  --option b2.connections=$B2_CONNECTIONS \
+  $BACKUP_EXCLUDES \
+  $BACKUP_PATHS &
 wait $!
 
 # Dereference and delete/prune old backups.
 # See restic-forget(1) or http://restic.readthedocs.io/en/latest/060_forget.html
 # --group-by only the tag and path, and not by hostname. This is because I create a B2 Bucket per host, and if this hostname accidentially change some time, there would now be multiple backup sets.
 restic forget \
-	--verbose \
-	--tag $BACKUP_TAG \
-	--option b2.connections=$B2_CONNECTIONS \
-        --prune \
-	--group-by "paths,tags" \
-	--keep-daily $RETENTION_DAYS \
-	--keep-weekly $RETENTION_WEEKS \
-	--keep-monthly $RETENTION_MONTHS \
-	--keep-yearly $RETENTION_YEARS &
+  --verbose \
+  --tag $BACKUP_TAG \
+  --option b2.connections=$B2_CONNECTIONS \
+  --prune \
+  --group-by "paths,tags" \
+  --keep-daily $RETENTION_DAYS \
+  --keep-weekly $RETENTION_WEEKS \
+  --keep-monthly $RETENTION_MONTHS \
+  --keep-yearly $RETENTION_YEARS &
 wait $!
 
 # Check repository for errors.
--- usr/local/sbin/restic_check.sh
+++ usr/local/sbin/restic_check.sh
@@ -15,7 +15,7 @@ exit_hook() {
 }
 trap exit_hook INT TERM
 
-
+# shellcheck disable=SC1091
 source /etc/restic/b2_env.sh
 
 # How many network connections to set up to B2. Default is 5.
